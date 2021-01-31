0f40dee88284353019a33ffbd5508c0e4057192f
hjhornbeck <hjhornbeck@protonmail.com>
2021-01-31 12:50:25 -0700

    - Minor tweak to the script.

--- update__did_dream_cheat.sh
+++ update__did_dream_cheat.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git checkout main    # restore ourselves to main
+
 # git remote add upstream https://github.com/hjhornbeck/bayes_speedrun_cheating
 git fetch upstream
 git merge upstream/main main
@@ -14,3 +16,5 @@ git checkout gh-pages
 git add _build/latex/python.pdf
 git commit -m "Auto-commit of LaTeX version."
 git push
+
+git checkout main    # restore ourselves to main
