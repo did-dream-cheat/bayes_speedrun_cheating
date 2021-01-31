602aed41a4ee12367332adc595b4a5f3479f34ee
hjhornbeck <hjhornbeck@protonmail.com>
2021-01-31 12:48:18 -0700

    - Added a helper script for the did-dream-cheat mirror.

---
+++ update__did_dream_cheat.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# git remote add upstream https://github.com/hjhornbeck/bayes_speedrun_cheating
+git fetch upstream
+git merge upstream/main main
+
+jupyter-book build .
+jupyter-book build --builder=pdflatex .
+
+# pip3 install --user ghp-import
+ghp-import -n -p -f _build/html
+
+git checkout gh-pages
+git add _build/latex/python.pdf
+git commit -m "Auto-commit of LaTeX version."
+git push
